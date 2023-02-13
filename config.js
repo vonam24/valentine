456f11a3ef6ad567603c1bdc95f0c9c4ad75d204
vonam24 <95601364+vonam24@users.noreply.github.com>
2023-02-13 11:58:23 +0700

    valentine

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Pà zà';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Valentine';
+const titleCard = 'pà zà iu dấu';
+const contentCard = '  Valentine chúc em luôn vui vẻ hạnh phúc với những quyết định của mình nhoaaa. chúc em ngày càng xinh đẹp nẻ Và em luôn là người anh iu thương nhất nhaaa.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'IMG_0903.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
